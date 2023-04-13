2452587560d8fce92835a4796b01ea438689e600
Arthur Henrique Caetano Santos <artyhenriq@unifei.edu.br>
2023-04-13 12:14:18 -0300

    Coin Formated and Label Name change when converted to BTC

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,29 +1,59 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { DropDownSecondCoin} from "./DropDownSecondValue";
+import { DropDownSecondCoin } from "./DropDownSecondValue";
 import style from "../styles/style.css";
 
-export function Converter({firstCoin}) {
-
+export function Converter({ firstCoin }) {
   const [firstValue, setFirstValue] = useState(0);
-  const [secondCoin, setSecondCoin] = useState('USD')
+  const [secondCoin, setSecondCoin] = useState("USD");
   const [price, setPrice] = useState(0);
 
   const changeFirstValue = function (e) {
     setFirstValue(e.target.value);
   };
 
+
+  if(secondCoin === 'BTC') {
+    let label = document.getElementById('labelCoin')
+    label.innerHTML = 'Moedas'
+  } else {
+    let label = document.getElementById('labelCoin')
+    label.innerHTML = firstCoin
+  }
+
   const convert = async function () {
     const url = `https://economia.awesomeapi.com.br/json/last/${secondCoin}-${firstCoin}`;
 
     const response = await fetch(url);
     const result = await response.json();
-    
+
     setPrice(result[`${secondCoin}${firstCoin}`]["bid"]);
     let finalValue = firstValue * price;
 
-    if (price) {
-      document.getElementById("answer").innerHTML = finalValue.toFixed(3);
+    if (secondCoin === "USD") {
+      const option = { style: "currency", currency: "USD" };
+      let finalValueFormated = new Intl.NumberFormat("en-US", option);
+      let finalAnswer = finalValueFormated.format(finalValue);
+
+      if (price) {
+        document.getElementById("answer").innerHTML = finalAnswer;
+      }
+    } else if (secondCoin === "EUR") {
+      const option = { style: "currency", currency: "EUR" };
+      let finalValueFormated = new Intl.NumberFormat("en-DE", option);
+      let finalAnswer = finalValueFormated.format(finalValue);
+
+      if (price) {
+        document.getElementById("answer").innerHTML = finalAnswer;
+      }
+    } else {
+      const option = { style: "currency", currency: "BRL" };
+      let finalValueFormated = new Intl.NumberFormat("pt-BR", option);
+      let finalAnswer = finalValueFormated.format(finalValue);
+
+      if (price) {
+        document.getElementById("answer").innerHTML = finalAnswer;
+      }
     }
   };
 
@@ -31,23 +61,23 @@ export function Converter({firstCoin}) {
     convert();
   }, []);
 
-  
-
   return (
     <div className="body">
       <div className="card">
         <h1 className="title">Coin Converter</h1>
-        <label>BRL</label>
+        <label id="labelCoin">BRL</label>
         <input
-          type="text"
+          type="number"
+          step={"0.01"}
+          min={"0.01"}
           onChange={changeFirstValue}
           value={firstValue}
           className="input-value"
           placeholder="Value"
         ></input>
-        <DropDownSecondCoin secondCoin={setSecondCoin}/>
+        <DropDownSecondCoin secondCoin={setSecondCoin} />
         <label id="answer"></label>
-        <button type="button" onClick={convert}>
+        <button type="reset" onClick={convert}>
           Converter
         </button>
       </div>
